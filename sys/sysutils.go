9ac22b1be5fd21dcaee928ca0a8d8e4583e80026
House.Lee <hlee@vendasta.com>
2017-04-19 17:49:08 -0600

    polishing the stub (but still a stub) and delete unnecessary (at least for now) network package

--- sys/sysutils.go
+++ sys/sysutils.go
@@ -1,7 +1,22 @@
 package sys
 
-func RegisterInheritFd() {
+type inheritFds struct {
+	listeners []int
+	files     []int
+}
 
+const (
+    ListenerFd = iota
+    FileFd
+)
+func RegisterInheritFd(fdType, fd int) {
+    //TODO
+    switch fdType {
+    case ListenerFd:
+    case FileFd:
+    default:
+
+    }
 }
 
 func GetInheritFds() {
