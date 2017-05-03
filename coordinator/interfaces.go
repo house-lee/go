b92932cbc7f72fe52f5b757dfb2bfc8bb98f2e1b
House.Lee <house.lee@usask.ca>
2017-05-03 00:26:08 -0600

    Coordinator stub

--- coordinator/interfaces.go
+++ coordinator/interfaces.go
@@ -7,4 +7,8 @@ import (
 type ICoordinator interface {
     NewRequest() (requestID string)
     UpdateRequestProgress(requestID string, workflow IWorkflow, workStation IWorkStation, job IJob, msg string) error
+    GetRequestProgress(requestID string) (workStationID string)
+    SaveRequestOutput(requestID string, output string, append bool) error
+    GetRequestOutput(requestID string) (output string, err error)
+    GetTaskList(workStationID string)//think about the return value
 }
