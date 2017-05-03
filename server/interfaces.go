92b5b8f7ff33f202855fbd21c506a045143e2a93
House.Lee <hlee@vendasta.com>
2017-05-03 17:42:17 -0600

    update server interface

--- server/interfaces.go
+++ server/interfaces.go
@@ -7,12 +7,14 @@ import (
 
 type IServer interface {
 	Serve(listener net.Listener) error
-	Stop() error
+	GracefulStop(maxTimeout int) error
+    Stop() error
 	RegisterWorkflow(routes []interface{}, workflow wkf.IWorkflow) error
 }
 
 type IUDPServer interface {
     Serve(conn *net.UDPConn)
+    GracefulStop(maxTimeout int) error
     Stop() error
     RegisterWorkflow(routes []interface{}, workflow wkf.IWorkflow) error
 }
