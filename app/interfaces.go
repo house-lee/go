5cd1fba2f6b69bcae64a24819801f2cecd1aa01d
House.Lee <hlee@vendasta.com>
2017-07-13 17:32:19 -0600

    add consts && update IServer, IApp interfaces

--- app/interfaces.go
+++ app/interfaces.go
@@ -5,13 +5,12 @@ import (
     "time"
 )
 
-type IRoute interface {
-
-}
+type IRoute interface {}
 
 type IServer interface {
     Init(serverID string, config interface{})
     RegisterWorkflow(routingRules IRoute, wf factory.IWorkflow)
+    FindWorkflow(route IRoute) (factory.IWorkflow, error)
     Serve()
     Stop() error
     GracefulStop(maxWaitTime time.Duration) error
@@ -19,7 +18,9 @@ type IServer interface {
 
 type IApp interface {
     RegisterServer(s IServer)
-    Run() error
+    //if maxRealThreads == 0, maxRealThreads = Num(Worker)
+    //elif maxRealThreads == -1, maxRealThreads = Num(CPU)
+    Run(maxRealThreads int32) error
     Shutdown() error
     GracefulShutdown(maxWaitTime time.Duration) error
     Restart(newBinaryPath string, maxWaitTime time.Duration) error
